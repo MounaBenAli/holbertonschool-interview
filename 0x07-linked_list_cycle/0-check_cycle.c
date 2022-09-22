45e77ba25453370db5d4de8e6584a6bc6b8a7c69
MounaBenAli <benalimouna1986@gmail.com>
2022-09-22 07:46:15 +0200

    Floyd's cycle detection algorithm

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * check_cycle -  checks if a singly linked list has a cycle(loop) in it.
+ * list: list to verify for cycle detection
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ * solution Floyd's cycle detection algorithm (Tortoise and hare)
+ */
+
+int check_cycle(listint_t *list)
+{
+
+	listint_t *slow_p = list;
+	listint_t *fast_p = list;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+		{
+		return (1);
+		}
+	}
+	return (0);
+}
