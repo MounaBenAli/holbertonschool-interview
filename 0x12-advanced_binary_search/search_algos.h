61ec82b3fffa3c6daf69f20359b94ff56ea536ae
MounaBenAli <benalimouna1986@gmail.com>
2022-12-15 14:52:12 +0100

    Header File

--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <stddef.h>
 
+
 int advanced_binary(int *array, size_t size, int value);
-int findFirstOccurnce(int a[], int n, int target);
+int findFirstOccurnce(int *arr, int low, int high, int value);
 void _printSearch(int *array, int start, int end);
 
 #endif /* _SEARCH_ALGO_H_*/
