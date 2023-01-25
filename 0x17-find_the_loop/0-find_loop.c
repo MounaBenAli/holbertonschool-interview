b1e9cace716ad744b82d0be1f4708108c83ab755
MounaBenAli <benalimouna1986@gmail.com>
2023-01-25 08:12:23 +0100

    Tortoise and Hare :turtle: && :rabbit2:

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop -  finds the loop/cycle in a linked list
+ * head: list to verify for cycle detection
+ * Return: if there is no cycle NULL,
+ * if there is a cycle the address of the node where the loop starts
+ * solution Floyd's cycle detection algorithm (Tortoise and hare)
+ * Tcomplexity O(n) Scomplexity O(1)
+ */
+
+
+listint_t *find_listint_loop(listint_t *head)
+{
+
+	listint_t *tortoise = head;
+	listint_t *hare = head;
+
+	if (!head)
+		return (NULL);
+
+
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+		{
+			break;
+		}
+	}
+
+	if (!hare || !hare->next)
+	{
+		return (NULL);
+	}
+
+	tortoise = head;
+	while (tortoise != hare)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next;
+	}
+
+	return (tortoise);
+}
