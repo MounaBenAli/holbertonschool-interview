adb04dca2a108e8b161ab7f0ae36f373224dc2ee
MounaBenAli <benalimouna1986@gmail.com>
2023-02-23 22:37:45 +0100

    fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """The Change Making Problem -
 Fewest Coins To Make Change
-Solved using Bottom-UP Dynamic Programming"""
+"""
 
 
 def makeChange(coins, total):
@@ -16,14 +16,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+    coins_used = 0
 
-    coins = sorted(coins)
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            coins_used += 1
 
-    for i in range(1, total + 1):
-        for j in range(0, len(coins)):
-            if coins[j] <= i:
-                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+    if total > 0:
+        return -1
 
-    return -1 if dp[total] > total else dp[total]
+    return coins_used
