6182ceb7f0f16a11c8d8f93688e392a0ec315641
MounaBenAli <benalimouna1986@gmail.com>
2023-02-23 15:05:03 +0100

    trying to sort the coins to reduce time complexity

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -19,6 +19,8 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
+    coins = sorted(coins)
+
     for i in range(1, total + 1):
         for j in range(0, len(coins)):
             if coins[j] <= i:
