fd937186e0d5fb675b8fa54731445da0fbea586b
MounaBenAli <benalimouna1986@gmail.com>
2023-02-23 13:24:29 +0100

    testing the fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -4,7 +4,7 @@ Fewest Coins To Make Change
 Solved using Bottom-UP Dynamic Programming"""
 
 
-def make_change(coins, total):
+def makeChange(coins, total):
     """Given a pile of coins of different values,
     determine the fewest number of coins needed
     to meet a given amount total.
@@ -13,24 +13,11 @@ def make_change(coins, total):
     If total is 0 or less, return 0
     If total cannot be met by any number of coins you have, return -1
     """
-    if total <= 0:
-        return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
-    # Create a table to store the minimum number of coins required
-    # to make up each value from 0 to the target value
-    table = [float('inf')] * (total + 1)
-    # Initialize the first entry of the table to 0
-    table[0] = 0
-
-    # Iterate over the coins and update the table entries
     for coin in coins:
         for i in range(coin, total + 1):
-            if table[i - coin] != float('inf'):
-                table[i] = min(table[i], table[i - coin] + 1)
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # Return the last entry in the table, which represents the
-    # minimum number of coins required to make up the target value
-    if table[total] == float('inf'):
-        return -1
-    else:
-        return table[total]
+    return dp[total] if dp[total] != float('inf') else -1
