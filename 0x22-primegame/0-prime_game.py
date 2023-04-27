ea202453a9c93055512ac3a11b18de276e02d200
MounaBenAli <benalimouna1986@gmail.com>
2023-04-27 17:28:56 +0200

    0. Prime Game

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Prime Game
+"""
+
+def isWinner(x, nums):
+    # create a list of prime numbers for each value of n in nums
+    primes = [set() for _ in range(max(nums) + 1)]
+    for i in range(2, len(primes)):
+        if len(primes[i]) == 0:
+            for j in range(i, len(primes), i):
+                primes[j].add(i)
+    # determine the winner for each round
+    maria_wins = 0
+    for n in nums:
+        if len(primes[n]) == 0:
+            # if there are no primes, Ben wins
+            maria_wins += 0
+        elif len(primes[n]) % 2 == 0:
+            # if the number of primes is even, Maria wins
+            maria_wins += 1
+        else:
+            # if the number of primes is odd, Ben wins
+            maria_wins += 0
+    if maria_wins > x - maria_wins:
+        # Maria wins the overall game
+        return "Maria"
+    elif maria_wins < x - maria_wins:
+        # Ben wins the overall game
+        return "Ben"
+    else:
+        # the winner cannot be determined
+        return None
